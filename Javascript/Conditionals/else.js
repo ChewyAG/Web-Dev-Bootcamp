610fc7dfd044ceb3cce2d0e773c4e06d6c3febf0
ChewyAG <ajgrant134@gmail.com>
2022-10-24 18:25:18 +0100

    Create else.js
    Learning "else" conditional

---
+++ Javascript/Conditionals/else.js
@@ -0,0 +1,11 @@
+const dayOfweek = prompt("Enter a day").toLowerCase();
+
+if (dayOfweek === "Monday") {
+     console.log("Ughh I hate Mondays!")
+} else if (dayOfweek === "Saturday") {
+     console.log("Yay, I love Saturdays!")
+} else if (dayOfweek === "Friday") {
+     console.log("Fridays are decent, especially after work")
+} else {
+     console.log("Meh")
+}
