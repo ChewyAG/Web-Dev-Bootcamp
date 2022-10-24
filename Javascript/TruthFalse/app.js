79485449ebf7540de0675ea85f14537163b2075f
ChewyAG <ajgrant134@gmail.com>
2022-10-24 18:30:30 +0100

    Create app.js
    Learning "truth" or "false" values

---
+++ Javascript/TruthFalse/app.js
@@ -0,0 +1,25 @@
+const userInput = prompt("Enter something");
+
+if(userInput){
+    console.log("Truth!")
+} else {
+    console.log("False!")
+}
+
+// if(0){
+//     console.log("Truth")
+//  } else {
+//      console.log("False!")
+//  }
+
+// if(NaN){
+//     console.log("Truth")
+//  } else {
+//      console.log("False!")
+//  }
+
+//  if(undefined){
+//     console.log("Truth")
+//  } else {
+//      console.log("False!")
+//  }
