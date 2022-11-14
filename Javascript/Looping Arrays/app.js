6d44a4a43e04397af87b5bcbd6dc1b4559dcc2d6
ChewyAG <ajgrant134@gmail.com>
2022-11-14 16:45:05 +0000

    Create app.js

---
+++ Javascript/Looping Arrays/app.js
@@ -0,0 +1,23 @@
+const animals = [
+    "Alpaca",
+    "Bear",
+    "Cat",
+    "Dog",
+    "Elephant",
+    "Fox",
+    "Gorilla",
+    "Hippo",
+    "Koala",
+]
+
+// List
+
+for (let i = 0; i <= animals.length; i++){
+    console.log(i, animals[i])
+}
+
+// Reverse List 
+
+for (let i = animals.length - 1; i >= 0; i--){
+    console.log(animals[i])
+}
