8f3ffdd228619d75c90c2212901cd127b909155c
ChewyAG <ajgrant134@gmail.com>
2022-11-14 16:44:10 +0000

    Create app.js

---
+++ Javascript/For Loops/app.js
@@ -0,0 +1,34 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// Number List
+
+for (let i = 1; i<=10; i++){
+    console.log(i);
+}
+
+// Increments of 2
+
+for (let i = 1; i <= 20; i += 2){
+    console.log(i);
+}
+
+// Reverse 
+
+for (let i = 100; i >= 0; i -= 10){
+    console.log(i);
+}
+
+// ==========================================
+//  DO NOT RUN THIS LOOP - INFINITE LOOP!
+//  for (let i = 20; i >= 0; i++){
+//      console.log(i)
+// }
