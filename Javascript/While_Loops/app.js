27f52f152107a1713c6ec437e67d4c4f99ecef16
ChewyAG <ajgrant134@gmail.com>
2022-11-16 18:12:15 +0000

    Update app.js
    Added "Guessing Game"

--- Javascript/While_Loops/app.js
+++ Javascript/While_Loops/app.js
@@ -21,3 +21,32 @@ while (true) {
     }
 
 console.log("Ok, you win")
+
+// Guessing Game
+
+let maximum = parseInt(prompt("Enter the maximum number"));
+while (!maximum) {
+    let maximum = parseInt(prompt("Enter a valid number"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+       guess = prompt("Too high, enter a new guess:");
+    } else {
+        guess = prompt("Too low, enter a new guess:");
+    }
+}
+if (guess === 'q') {
+    console.log("Ok, you quit")
+} else {
+    console.log("Congrats, you win")
+    console.log(`You got it, it took you ${attempts} guesses`)
+}
