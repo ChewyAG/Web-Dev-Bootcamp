632cd956622d838bef2cf77825ab399e6c4b8c13
ChewyAG <ajgrant134@gmail.com>
2022-12-05 11:15:25 +0000

    Create app.js

---
+++ Javascript/IntrotoFunctions/app.js
@@ -0,0 +1,24 @@
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+singSong()
+singSong()
+singSong()
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`);
+}
+
+
+function repeat(str, numTimes) {
+    let result = "";
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+
